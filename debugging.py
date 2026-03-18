2e8612198a2fca0eafe8233afbb99b7df167d300
Mariana F <marianafama@unisabana.edu.co>
2026-03-17 21:18:56 -0500

    1

--- debugging.py
+++ debugging.py
@@ -4,25 +4,29 @@
 def get_daily_steps():
     """Return list of daily steps."""
     steps = input("Enter your daily steps for 7 days separated by spaces: ")
-    step_list = steps.split
-    step_list = [int(s) for s in step_list]
-    return step_list
+    step_list = steps.split()
+    step_list2 = []
+    for s in step_list:
+        step_list2.append(int(s))
+    return step_list2
 
 # Function to calculate total steps
 def total_steps(nums):
     """Return total steps."""
     total = sum(nums)
+    return total
 
 # Function to calculate average daily steps
-def average_steps(total, days=7):
+def average_steps(total =0, days=7):
     """Return average steps as int."""
-    return total / days
+    avarage = total //days
+    return avarage
 
 # Function to get maximum steps
 def max_steps(nums):
     """Return max steps."""
-    max_val = max(nums)
-    return maxvalue
+    max_val_max(nums)
+    return max_val
 
 # Function to get minimum steps
 def min_steps(nums):
@@ -35,9 +39,9 @@ def goal_check(nums, goal=10000):
     result = []
     for s in nums:
         if s >= goal:
-            result.append("True")
+            result.append(True)
         else:
-            result.append("False")
+            result.append(False)
     return result
 
 # ----------------------
@@ -46,7 +50,7 @@ def goal_check(nums, goal=10000):
 step_list = get_daily_steps()
 
 total = total_steps(step_list)
-average = average_steps(total)
+average = average_steps(total, int(7))
 highest = max_steps(step_list)
 lowest = min_steps(step_list)
 goal_met = goal_check(step_list)
