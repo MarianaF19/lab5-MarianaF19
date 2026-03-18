8ae42b9900f262e6f05bed2f26d71f15c7e9021b
Mariana F <marianafama@unisabana.edu.co>
2026-03-17 21:50:41 -0500

    5

--- secret_number.py
+++ secret_number.py
@@ -3,5 +3,5 @@ import random
 def seed_secret_numbers(seed):
     return random.seed(seed)
 
-def generate_secret_numbers(star=1, end=100):
+def generate_secret_numbers(start=1, end=100):
     return random.randint(start,end)
